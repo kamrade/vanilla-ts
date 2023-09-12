f445d24644621417bfef9de36126d04a77ea6872
Dennis Michailov <denis.michailov@gmail.com>
2023-09-12 10:27:40 +0300

    Console

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -1,6 +1,6 @@
 export const log = (app: HTMLDivElement | null, content: string) => {
   const el: HTMLDivElement = document.createElement('div');
-  el.classList.add('text-center')
+  el.classList.add('log')
   el.innerText = content;
   app?.appendChild(el);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import './css/style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app');
 log(app, 'Some inner text 1');
+log(app, 'Some other text 2');
+log(app, 'Some another new inner text 3');
 
